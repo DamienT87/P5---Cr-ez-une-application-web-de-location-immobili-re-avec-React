0cadeab1ceb1990ec943aa92ff77db90d26e9ea4
DamienT87 <taillemaudamien87@gmail.com>
2025-11-01 19:43:36 +0100

    mise en place d'un css pour le header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 import "./Header.scss";
+import logo from "../assets/images/logo_Kasa.png";
 
 export default function Header(){
     return(
         <header>
+            <div className="logo">
+                <img src={logo}/>
+            </div>
             <nav>
                 <ul>
                     <li>
