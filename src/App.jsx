7cebb069101ba8dfc26a5db33b6ba4e71ec9fa09
DamienT87 <taillemaudamien87@gmail.com>
2025-11-01 18:57:48 +0100

    fix du bug APropos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import AppRouter from "./router/index.jsx";
 
 function App() {
 
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "../pages/Home";
-import Apropos from "../pages/APropos";
+import Apropos from "../pages/APropos.jsx";
 import Logement from "../pages/Logement";
 import NotFound from "../pages/NotFound";
 
@@ -8,7 +8,7 @@ export default function AppRouter(){
     return(
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/apropos" element={<APropos />} />
+            <Route path="/apropos" element={<Apropos />} />
             <Route path="/logement/:id" element={<Logement />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
